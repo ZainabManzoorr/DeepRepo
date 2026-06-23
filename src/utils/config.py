e09cf4284a044874a0cc95b3e0ae186b56d924a1
Zainab Manzoor <zainabmanzoorzoor@gmail.com>
2026-06-23 19:03:52 +0500

    Updated config

---
+++ src/utils/config.py
@@ -0,0 +1,14 @@
+SUPPORTED_EXTENSIONS = {
+    ".py",
+    ".js",
+    ".ts",
+    ".md"
+}
+
+IGNORED_DIRECTORIES = {
+    ".git",
+    "node_modules",
+    "__pycache__"
+}
+
+MODEL_NAME = "llama-3.1-8b-instant"
