4702d27ecf6be36c1449cd79c7980b9d806ac2c7
Zainab Manzoor <zainabmanzoorzoor@gmail.com>
2026-07-06 21:00:26 +0500

    Added sybmol table

---
+++ src/graph/symbol_table.py
@@ -0,0 +1,37 @@
+class SymbolTable:
+  
+  def __init__(self):
+     
+       self.symbols = {}
+  def add_symbol(
+        self,
+        name: str,
+        file: str,
+        symbol_type: str,
+        line: int
+  ):
+    self.symbols[name] = {
+        "file": file,
+        "type": symbol_type,
+        "line": line
+    }
+  def lookup(
+        self,
+        name: str
+  ):
+    return self.symbols.get(name)
+  
+  def exits(
+        self,
+        name: str
+  ) -> bool:
+    return name in self.symbols
+  def all_symbols(self):
+    return self.symbols
+  def clear(self):
+    self.symbols.clear()
+    
+  def __len__(self):
+    return len(self.symbols)
+  
+print("\nSYMBOL TABLE")
